0ca3ed3d7249b823cd060cd85ed23cfa4fed56e0
Alexandre Ferreira <alexandresgf@gmail.com>
2020-01-21 22:39:21 -0300

    Add the TakeFirst output processor as default

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/loaders.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/loaders.py
@@ -6,9 +6,11 @@
 # https://docs.scrapy.org/en/latest/topics/loaders.html
 
 from scrapy.loader import ItemLoader
+from scrapy.loader.processors import TakeFirst
 
 from {{cookiecutter.project_slug}}.items import {{cookiecutter.scrapy_project_main_class}}Item
 
 
 class {{cookiecutter.scrapy_project_main_class}}ItemLoader(ItemLoader):
     default_item_class = {{cookiecutter.scrapy_project_main_class}}Item
+    default_output_processor = TakeFirst()
